e1652269e9e8b5975cee7fd6e65592972c871266
Kristhal <Kristhal@192.168.56.1>
2017-03-04 21:15:43 -0600

    Hoja de trabajo No. 2 Nombre: Kristhal Jasmine Meng Marroquin Carnet: 201314655

--- src/org/compi2/codigo3d/Inicio.java
+++ src/org/compi2/codigo3d/Inicio.java
@@ -15,7 +15,7 @@ public class Inicio extends javax.swing.JFrame {
     public Inicio() {
         initComponents();
     }
-
+    //LA HOJA DE TRABAJO ESTA TERMINADA
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
